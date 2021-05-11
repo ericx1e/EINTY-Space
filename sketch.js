d34217fef5098f6988dc988c9985b3bd1ec80792
ericx1e <ericxie6@gmail.com>
2021-05-11 14:57:09 -0400

    text?

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
+let _text;
+
 function setup() {
-    img = loadImage('assets/milkyway.jpg');
+    backgroundImg = loadImage('assets/milkyway.jpg');
     img1 = loadImage('assets/earth.jpg');
     img2 = loadImage('assets/sun.jpg');
     img3 = loadImage('assets/moon.jpg');
@@ -7,24 +9,40 @@ function setup() {
     canvas.position(0, 0);
     rover = createRoverCam();
     rover.usePointerLock();
-    rover.setState({position: [-400,-0,-200], rotation: [0.4,0.3,0], sensitivity: 0.025, speed: 1.0});
+    // rover.setState({position: [-400,-0,-200], rotation: [0.4,0.3,0], sensitivity: 0.025, speed: 1.0});
+    defaultView();
+
+
+  _text = createGraphics(window.innerWidth - 4, window.innerHeight - 4);
+  _text.clear();
+//   _text.textFont('Source Code Pro');
+  _text.textAlign(CENTER);
+  _text.textSize(133);
+  _text.fill(255);
+  _text.noStroke();
+  _text.text('test', width * 0.5, height * 0.5);
 }
+
 
 let orbit = 0;
 let rotation = 0;
+let scene = 0;
+
 
 function draw() {
     background(0);
     push();
-    // translate(width/2, height/2);
-    // orbitControl();
-    // rotateY(rotation);
-    texture(img)
+    translate(0, 0, 0);
+    texture(backgroundImg);
     noStroke();
-    // box(200);
-    sphere(500)
-    // fill(100);
-    // box(50);
+    sphere(500);
+    
+    // fill(255);
+    rotateY(-PI/2);
+    texture(_text);
+    rectMode(CENTER, CENTER);
+    rect(0, -100, 100, 100);
+    text('HELLO', 0, 0);
     lights();
     lightFalloff(0.5, 0, 0);
     pointLight(255, 255, 255, 0, 0, 0);
@@ -45,4 +63,14 @@ function draw() {
     pop();
     rotation+=0.05;
     orbit += 0.01;
+}
+
+function keyPressed() {
+    if(key == ' ') {
+        defaultView();
+    }
+}
+
+function defaultView() {
+    rover.setState({position: [-350,-300,0], rotation: [0,0.7,0], sensitivity: 0.025, speed: 1.0});
 }
